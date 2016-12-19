81c652d1adc40d737d4234a9cdc6210327de872a
Sina Kashi Pazha <esterlinkof@gmail.com>
2016-12-20 02:04:21 +0330

    refactor code.

--- digistyle.js
+++ digistyle.js
@@ -7,7 +7,7 @@ var options = {
     method: 'GET'
 };
 
-var req = http.request(options, function (res) {
+http.request(options, function (res) {
     var data = "";
 
     res.setEncoding('utf8');
@@ -16,21 +16,13 @@ var req = http.request(options, function (res) {
     });
     res.on('end', function () {
         var obj = JSON.parse(data);
-        // console.log(obj.hits.hits[0]);
-        // console.log(obj.hits.hits[0]._source);
-        // console.log(obj.hits.hits[0]._source.MinPrice);
-
-        // var temp = obj.hits.hits[0];
-        // open("http://www.digistyle.com/Product/"+temp._source.FaTitle.replace(' ', '-')+'-'+temp._id);
         var temp;
         for (var i = 0; i < obj.hits.hits.length; i++) {
             temp = obj.hits.hits[i];
             if (temp._source.MinPrice < 10000 && temp._source.MinPrice > 0) {
-            // if (temp._source.MinPrice == 1710000 ) {
                 console.log(temp);
                 open("http://www.digistyle.com/Product/" + temp._source.FaTitle.replace(' ', '-') + '-' + temp._id);
             }
         }
     });
 }).end();
-
