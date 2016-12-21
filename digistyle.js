ff27521fdceeccadfaf8245fadecb55246417361
Sina Kashi Pazha <esterlinkof@gmail.com>
2016-12-21 11:13:55 +0330

    Handle these error: can't parse JSON and play sound when find new stuff.

--- digistyle.js
+++ digistyle.js
@@ -1,36 +1,47 @@
 var http = require('http');
 var open = require('open');
+var player = require('play-sound')(opts = {});
 
 var options = {
     hostname: 'search.digistyle.com',
     path: '/api2/Data/GetCollection?collectionId=1&pageIndex=1&pageSize=10000',
     method: 'GET'
 };
+var counter = 1;
 
-var req = http.request(options, function (res) {
+
+var requestHandler = function (res) {
     var data = "";
+    console.log("counter is: " + counter);
+    counter++;
 
     res.setEncoding('utf8');
     res.on('data', function (chunk) {
         data += chunk;
     });
     res.on('end', function () {
-        var obj = JSON.parse(data);
-        // console.log(obj.hits.hits[0]);
-        // console.log(obj.hits.hits[0]._source);
-        // console.log(obj.hits.hits[0]._source.MinPrice);
-
-        // var temp = obj.hits.hits[0];
-        // open("http://www.digistyle.com/Product/"+temp._source.FaTitle.replace(' ', '-')+'-'+temp._id);
+        var obj;
+        try {
+            obj = JSON.parse(data);
+        }catch (e){
+            setTimeout(http.request(options,requestHandler).end(), 30000);
+            return;
+        }
         var temp;
+        var find = false;
         for (var i = 0; i < obj.hits.hits.length; i++) {
             temp = obj.hits.hits[i];
-            if (temp._source.MinPrice < 10000 && temp._source.MinPrice > 0) {
-            // if (temp._source.MinPrice == 1710000 ) {
+            if (temp._source.MinPrice <= 10000 && temp._source.MinPrice > 0) {
                 console.log(temp);
+                find = true;
                 open("http://www.digistyle.com/Product/" + temp._source.FaTitle.replace(' ', '-') + '-' + temp._id);
+                player.play('1.mp3');
             }
         }
+        if (!find) {
+            setTimeout(http.request(options,requestHandler).end(), 10000);
+        }
     });
-}).end();
+};
 
+setTimeout(http.request(options,requestHandler).end(), 1);
